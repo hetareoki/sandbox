0fca5ef8391f647199a33dced4079c5f5af34e67
KENTA OKI <hetare.oki@gmail.com>
2012-08-15 23:48:12 +0900

    Add a C#/99.cs

---
+++ C#/99.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Kuku {
+	public static void Main() {
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 1; j <= 9; j++) {
+				Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+			}
+		}
+	}
+}
